152275d2a3762cc55cbafd1109692c097d7c610f
tonybaroneee <tonybaroneee@gmail.com>
2012-10-20 01:26:40 -0400

    Added style filter, app is fully functional, will be adding the extra pages to view all wines by category

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,9 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Wine;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
@@ -15,22 +13,48 @@ public class Application extends Controller {
     
     public static void getWineList() {
         String price = request.params.get("price");
+        String style = request.params.get("style");
 
         List<Wine> wineList;
         StringBuilder query = new StringBuilder("");
+        String priceQuery = "";
+        String styleQuery = "";
+        String andQuery = "and ";
+        
+        // See if there is a price constraint
         if (price.equals("low")) {
-            query = query.append("price < ? order by price asc");
-            wineList = Wine.find(query.toString(), 11).fetch();
+            priceQuery = "price < 11 ";
         } else if (price.equals("mid")) {
-            query = query.append("price between ? and ? order by price asc");
-            wineList = Wine.find(query.toString(), 10, 18).fetch();
+            priceQuery = "price between 10 and 17 ";
         } else if (price.equals("high")) {
-            query = query.append("price > ? order by price asc");
-            wineList = Wine.find(query.toString(), 17).fetch();
+            priceQuery = "price > 17 ";
         } else {
-            wineList = Wine.find("order by price asc").fetch();
+            // Do nothing
         }
         
+        // See if there is a style constraint
+        if (style.equals("1")) {
+            styleQuery = "style is 1 ";
+        } else if (style.equals("2")) {
+            styleQuery = "style is 2 ";
+        } else if (style.equals("3")) {
+            styleQuery = "style is 3 ";
+        } else {
+            // Do nothing
+        }
+        
+        // Let's build the query
+        if (priceQuery.length() > 0 && styleQuery.length() > 0) {
+            query.append(priceQuery + andQuery + styleQuery);
+        } else if (priceQuery.length() > 0) {
+            query.append(priceQuery);
+        } else if (priceQuery.length() > 0) {
+            query.append(styleQuery);
+        }
+        query = query.append("order by price asc");
+        
+        // Finally, fetch the list of wines and return the result as a JSON object
+        wineList = Wine.find(query.toString()).fetch();
         renderJSON(wineList);
     }
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,40 @@
 $(function () {
 
+  // Hide the address bar in mobile safari (iOS5)
+  if (navigator.userAgent.match(/OS 5(_\d)+ like Mac OS X/i)) {
+    setTimeout(function(){
+      window.scrollTo(0, 1);
+    }, 1000);
+  }
+
+  // Initialize the qTip tooltip plugin, but dont bother if it's a mobile/touchscreen device
+  if (!navigator.userAgent.match(/Android/i) &&
+    !navigator.userAgent.match(/webOS/i) &&
+    !navigator.userAgent.match(/iPhone/i) &&
+    !navigator.userAgent.match(/iPod/i) &&
+    !navigator.userAgent.match(/iPad/i) &&
+    !navigator.userAgent.match(/Blackberry/i)) {
+    // Mobile browser not detected, enable tooltips
+    $('body').tooltip({
+      selector: '[rel=tooltip]'
+    });
+  }
+
   /******************************************/
   /* HTML Templates                         */
   /******************************************/
 
-  var wineResultsHeader = '<h2 class="results-header">Hooray! We\'ve found some wines just for you:</h2>';
-  var wineNoResultsHeader = '<h2 class="results-header">Sorry, we couldn\'t find any wines that match your choices.</h2>';
+  var wineResultsHeader = '<div id="wine-list-header-container" class="container"><h2 class="results-header">Hooray! We\'ve found some wines just for you:</h2><a id="back-to-search" style="float:right" href="#">Modify your search</a></div>';
+  var wineNoResultsHeader = '<div id="wine-list-header-container" class="container"><h2 class="results-header">Sorry, we couldn\'t find any wines that match your choices.</h2><a id="back-to-search" style="float:right" href="#">Modify your search</a><div>';
   var wineRow = '<div class="container well">' +
-                  '<img class="bottle-img img-polaroid">' +
+                  '<div class="bottle-img-container"><img class="bottle-img img-polaroid"></div>' +
                   '<div class="wine-info-box">' +
                     '<h1 class="wine-price"></h1>' +
+                    '<h4 class="wine-alc"></h4>' +
                     '<h3 class="wine-title"></h3>' +
                     '<h5 class="wine-location"></h5>' +
+                    '<h5 class="wine-type"></h5><hr class="soften wine-row-line"></hr>' +
+                    '<p class="wine-desc"></p>' +
                   '</div>' +
                 '</div>';
   var resetBtn = '<div class="container" style="text-align:center;"><button id="resetsearch" class="btn btn-primary btn-large bigbutton">Start A New Search</button></div>';
@@ -28,6 +51,15 @@ $(function () {
     }
   }
 
+  function swapChoice(button, categoryName) {
+    $('.'+categoryName+'-btn').removeClass('btn-success');
+    $('.'+categoryName+'-text').removeClass('chosen');
+    $('.'+categoryName+'-caption').removeClass('chosen');
+    button.addClass('btn-success');
+    button.siblings('.'+categoryName+'-text').addClass('chosen');
+    button.siblings('.'+categoryName+'-caption').addClass('chosen');
+  }
+
   /******************************************/
   /* Initializations and Event Listeners    */
   /******************************************/
@@ -39,23 +71,24 @@ $(function () {
 
   $('.price-btn').on('click', function(e) {
     var $this = $(this);
-    $('.price-btn').removeClass('btn-success');
-    $('.price-text').removeClass('chosen');
-    $this.addClass('btn-success');
-    $this.siblings('.price-text').addClass('chosen');
+    swapChoice($this, 'price');
+  });
+
+  $('.style-btn').on('click', function(e) {
+    var $this = $(this);
+    swapChoice($this, 'style');
   });
 
   $('#submitsearch').on('click', function(e) {
     e.preventDefault();
-    $(this).fadeOut();
 
     var wineList = $('#wine-list');
     wineList.empty();
 
     $.getJSON('/getwinelist', {
-      price : $('.price-btn.active').val() || ''
+      price : $('.price-btn.active').val() || '',
+      style : $('.style-btn.active').val() || ''
     }, function(data) {
-      console.log(data)
       if (data.length > 0) {
         wineList.append(wineResultsHeader)
       } else {
@@ -76,23 +109,40 @@ $(function () {
             .text('$' + wine.price)
             .end()
           .find('.wine-location')
-            .html('<i class="icon-globe"></i> ' + wine.location);
+            .html('<i class="icon-globe"></i> ' + wine.location)
+            .end()
+          .find('.wine-type')
+            .html('<i class="icon-glass"></i> ' + wine.type)
+            .end()
+          .find('.wine-alc')
+            .text(wine.alc + '% alc.')
+            .end()
+          .find('.wine-desc')
+            .text(wine.description);
 
           wineList.append(wineRowHTML.get(0).outerHTML);
         }
       }
       wineList.append(resetBtn);
-      wineList.fadeIn(function() {
+      if (wineList.is(':hidden')) {
+        wineList.fadeIn(function() {
+          goToByScroll('wine-list');
+        });
+      } else {
         goToByScroll('wine-list');
-      });
+      }
     });
+  });
+
+  $('#wine-list').on('click', '#back-to-search', function(e) {
+    e.preventDefault();
+    goToByScroll('search-start');
   });
 
   $('#wine-list').on('click', '#resetsearch', function(e) {
     e.preventDefault();
     goToByScroll("search-start");
     $('#wine-list').fadeOut('fast', function() {
-      $('#submitsearch').fadeIn('fast');
     });
   });
 
